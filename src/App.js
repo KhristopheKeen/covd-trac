13f6da86d501a82181bf654b0d2472662bb662e4
PortraitstoneLabs <khristophe.keen@gmail.com>
2022-02-05 12:45:15 -0800

    Refactored

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './App.module.css';
 import { fetchData } from './api';
-
 import { Cards, Chart, CountryPicker } from './components';
+import coronaImage from './images/image.png'
 
 class App extends React.Component {
     state = {
@@ -26,6 +26,7 @@ class App extends React.Component {
         const { data, country } = this.state;
         return (
             <div className={styles.container}>
+                <img className={styles.image} src={coronaImage} alt="COVID-19"/>
                 <Cards data={data}/>
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
                 <Chart data={data} country={country}/>
